73fbd78c70328307745f455bb345ac8c981ac7c9
anderson-matheus <matheus5347@gmail.com>
2020-02-05 18:00:03 -0300

    ajustando mensagens

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ espaço de tempo, mas apenas se esta operação dê lucro. Para isso, é passado
 K com os valores das ações nos determinados dias, onde ele poderá escolher onde comprar e vender.\n""")
 
     try:
-        values = list(int(num) for num in input("Informe números inteiros separados por vírgula: ").strip().split())
+        values = list(int(num) for num in input("Informe os valores das ações separados por espaço: ").strip().split())
     except:
-        raise Exception('Informe valores inteiros separados por espaço')
+        print('Informe os valores das ações separados por espaço')
+        sys.exit()
 
     profit = Profit(values)
-    print('\nO maior lucro possível de compra e venda é: %d\n\n' % profit.calculate_profit())
+    print('\nO maior lucro possível será de: %d\n\n' % profit.calculate_profit())
 
     message = input('\nDigite (sim) se deseja calcular o lucro com outros valores: ')
     if (message.lower() != 'sim'):
